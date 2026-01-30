88c8d0a9b16fd2f3e27ebbea38702e97faab86b7
Adrian R. Esguerra <i.am.adriansgrrx@gmail.com>
2026-01-31 00:04:15 +0800

    feat: implement Navbar and Menu components; update Home page layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import Menu from '../components/Menu';
+import Navbar from '../components/Navbar';
 
 const Home = ( {user, setUser, error} ) => {
   return (
-    <div>Home</div>
+    <div className='min-h-screen bg-amber-50'>
+      <Navbar />
+      Home
+      <Menu />
+    </div>
   )
 }
 
